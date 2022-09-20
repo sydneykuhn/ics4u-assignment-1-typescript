355348b2b03e9f000f846e68f74c8ff75c1245ed
Sydney Kuhn <sydney.kuhn@mths.ca>
2022-09-20 03:18:27 +0000

    added const

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,11 @@ const userInputString = prompt('Enter a number 1, 2 or 3 to chose : ')
 
 const userInput = parseFloat(userInputString)
 
+// Set constants
+const rock = 1
+const paper = 2
+const scissor = 3
+
 // Gets random int
 const min = 1
 const max = 3
@@ -25,17 +30,17 @@ const randomInt = Math.floor(Math.random() * max + min)
 
 if (userInput === randomInt) {
   console.log('You tied!')
-} else if (userInput === 1 && randomInt === 2) {
+} else if (userInput === rock && randomInt === paper) {
   console.log('You lose!')
-} else if (userInput === 1 && randomInt === 3) {
+} else if (userInput === rock && randomInt === scissor) {
   console.log('You win!')
-} else if (userInput === 2 && randomInt === 1) {
+} else if (userInput === paper && randomInt === rock) {
   console.log('You win!')
-} else if (userInput === 2 && randomInt === 3) {
+} else if (userInput === paper && randomInt === scissor) {
   console.log('You lose!')
-} else if (userInput === 3 && randomInt === 1) {
+} else if (userInput === scissor && randomInt === rock) {
   console.log('You lose!')
-} else if (userInput === 3 && randomInt === 2) {
+} else if (userInput === scissor && randomInt === paper) {
   console.log('You win!')
 } else {
   console.log('\nInvalid Input Entered.\nDone.')
@@ -43,11 +48,11 @@ if (userInput === randomInt) {
 }
 
 // Show user what the computer chose
-if (randomInt === 1) {
+if (randomInt === rock) {
   console.log('\nThe computer chose rock.')
-} else if (randomInt === 2) {
+} else if (randomInt === paper) {
   console.log('\nThe computer chose paper.')
-} else if (randomInt === 3) {
+} else if (randomInt === scissor) {
   console.log('\nThe computer chose scissors.')
 } else {
   console.log('\nError Occured Please Try Again.')
